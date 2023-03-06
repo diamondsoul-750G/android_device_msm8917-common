c653ab6ff5f95691b7d1f951e4d7b8fc65c018a4
diamondsoul-750G <n.shetti65@gmail.com>
2023-03-06 15:17:33 +0530

    bluetooth support for more devices

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,7 +22,26 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Galaxy J6+"
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char *BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strcmp(product_device, "j6primelte") == 0)
+        return "Samsung Galaxy J6+";
+    if (strcmp(product_device, "j4primelte") == 0)
+        return "Samsung Galaxy J4+";
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
@@ -31,4 +50,5 @@
 #define BT_CLEAN_TURN_ON_DISABLED 1
 // Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
+#pragma pop_macro("PROPERTY_VALUE_MAX")
 #endif
