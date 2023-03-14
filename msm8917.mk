13d4af214b9af20e36533c5365ac7e50b421f3d2
diamondsoul-750G <n.shetti65@gmail.com>
2023-03-14 11:14:26 +0530

    trying to fix fingerprint and camera

--- msm8917.mk
+++ msm8917.mk
@@ -239,6 +239,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor \
     android.hidl.manager@1.0-java \
     libbacktrace_vendor
 
