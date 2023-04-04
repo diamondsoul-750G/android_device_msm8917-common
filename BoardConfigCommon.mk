b3516a70c1c067439198b3aa635289a3e019498b
diamondsoul-750G <n.shetti65@gmail.com>
2023-04-04 23:04:12 +0530

    fingerprint fixed

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Qcom Hardware
+OVERRIDE_QCOM_HARDWARE_VARIANT := msm8996-los
 
 COMMON_PATH := device/samsung/msm8917-common
 BOARD_VENDOR := samsung
